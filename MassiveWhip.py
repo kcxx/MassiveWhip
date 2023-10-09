6278bc548114a2d59306e8c755798ffcd1eba0c6
kcxx <96025955+kcxx@users.noreply.github.com>
2023-10-09 13:21:20 +0200

    Update MassiveWhip.py

--- MassiveWhip.py
+++ MassiveWhip.py
@@ -8,153 +8,126 @@ Created on Mon Oct 12 16:00:22 2020
 import os
 import discord
 import random
+import aiohttp
 
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
-from discord.ext import commands
+from datetime import datetime
 from tabulate import tabulate
+
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents, command_prefix='/')
+        self.tree = discord.app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=GUILD)
+        await self.tree.sync(guild=GUILD)
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 OWNER = os.getenv('OWNER_ID')
+GUILD = discord.Object(id=os.getenv('GUILD_ID'))
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.members = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
-signedReactions = ['Warrior', 'Druid', 'Paladin', 'Rogue', 'Hunter', 'Mage', 'Warlock', 'Priest', 'Shaman', 'Bench',
-                   'Late', 'Tentative', 'Absence']
+client = MyClient(intents=intents)
+
+
+@client.event
+async def on_ready():
+    await client.get_user(int(OWNER)).send('I am ready')
+
+
+def is_vedeni():
+    def predicate(interaction: discord.Interaction) -> bool:
+        role = discord.utils.get(interaction.guild.roles, name='vedeni')
+        return role in interaction.user.roles
+
+    return discord.app_commands.check(predicate)
+
+
+def log_command(command, author):
+    print(datetime.now(), command, '\t', author)
+
+
+def list_diff(li1, li2):
+    return list(list(set(li1) - set(li2)) + list(set(li2) - set(li1)))
+
+
+def list_subtract(li1, li2):
+    return list(list(set(li1) - set(li2)))
+
+
+@client.tree.context_menu(name='Whip')
+@is_vedeni()
+async def whip(interaction: discord.Interaction, message: discord.Message):
+    log_command('whip', interaction.user.name)
+
+    async with interaction.channel.typing():
+        url = f'https://raid-helper.dev/api/v2/events/{message.id}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                if r.status != 200:
+                    return await interaction.response.send_message(f'Raid-Helper server error.', ephemeral=True)
+
+                js = await r.json()
+                if 'status' in js and js['status'] == 'failed':
+                    return await interaction.response.send_message(f'Raid-Helper event [{message.id}] not found.',
+                                                                   ephemeral=True)
+
+                role = discord.utils.get(interaction.guild.roles, name='raider')
+                all_raiders = [m.display_name for m in role.members]
+                role = discord.utils.get(interaction.guild.roles, name='vedeni')
+                all_raiders.extend([m.display_name for m in role.members])
+                all_raiders = list(set(all_raiders))
+
+                signed = [m['name'] for m in js['signUps']]
+                unsigned = list_diff(all_raiders, signed)
+                unsigned_raiders = [m for m in role.members if m.display_name in unsigned]
+                mention = False
+
+                if not unsigned_raiders:
+                    msg = ['You\'ll call me back. I know it.']
+                else:
+                    msg = ['Finally, a chance to demonstrate my... talents...\n']
+                    for ur in unsigned_raiders:
+                        msg.extend([ur.mention if mention else ur.display_name, ' '])
+                    msg.extend(['\nShall we begin?'])
+
+                await interaction.response.send_message(''.join(msg))
+
+
 councilRoleSize = 7
 councilVedeniCount = 3
 councilCoreCount = 2
 
 
-@bot.event
-async def on_ready():
-    await bot.get_user(int(OWNER)).send('I am ready')
+@client.tree.command(name='council',
+                     description='Generate new loot council based on the discord roles (vedeni/raider).')
+@is_vedeni()
+async def council(interaction: discord.Interaction):
+    log_command('council', interaction.user.name)
+    async with interaction.channel.typing():
+        vedeni = discord.utils.get(interaction.guild.roles, name='vedeni').members
+        core = list_subtract(discord.utils.get(interaction.guild.roles, name='raider').members, vedeni)
 
+        c_vedeni = random.sample(vedeni, k=councilRoleSize)
+        c_vedeni = [m.display_name for m in c_vedeni]
+        c_vedeni = c_vedeni[:councilVedeniCount] + [''] + c_vedeni[councilVedeniCount:]
 
-def is_eligible_to_whip():
-    async def predicate(ctx):
-        vedeniRole = await commands.RoleConverter().convert(ctx, 'vedeni')
-        return vedeniRole in ctx.author.roles
+        c_core = random.sample(core, k=councilRoleSize - 1)
+        c_core = [m.display_name for m in c_core]
+        c_core = c_core[:councilCoreCount] + ['', ''] + c_core[councilCoreCount:]
 
-    return commands.check(predicate)
-
-
-def parse_raidhelper_event_datetime(embedFields):
-    token = embedFields[-1].value.split('&dates=', 1)[1].split('Z', 1)[0]
-    token_no_delim = ''.join(token.split('T', 1))
-    event_datetime = datetime.strptime(token_no_delim, '%Y%m%d%H%M%S') + timedelta(hours=1)
-    return event_datetime
-
-
-def is_upcoming_event(msg):
-    return datetime.utcnow() < parse_raidhelper_event_datetime(msg.embeds[0].fields)
-    
-    
-def log_command(command, author):
-    print(datetime.now(), command, '\t', author)
-    
-
-async def getUnsignedMembers(ctx):
-    vedeniRole = await commands.RoleConverter().convert(ctx, 'vedeni')
-    raiderRole = await commands.RoleConverter().convert(ctx, 'raider')
-    # clenRole = await commands.RoleConverter().convert(ctx, 'clen')
-    allRaiders = set(vedeniRole.members + raiderRole.members)
-    unsignedRaiders = set()
-
-    async for msg in ctx.channel.history(limit=5):
-        if msg.author.name == 'Raid-Helper' and is_upcoming_event(msg):
-            signedUsersSet = set()
-            for r in msg.reactions:
-                if r.emoji.name in signedReactions:
-                    signedUsersSet.update(set(await r.users().flatten()))
-
-            signedRaidersSet = set()
-            for u in signedUsersSet:
-                signedMember = ctx.guild.get_member(u.id)
-                if vedeniRole in signedMember.roles or raiderRole in signedMember.roles:
-                    signedRaidersSet.add(signedMember)
-
-            unsignedRaiders.update(allRaiders.difference(signedRaidersSet))
-
-    return unsignedRaiders
-
-
-async def getUnsignedMembersMsg(ctx, mention=True):
-    unsignedRaiders = await getUnsignedMembers(ctx)
-    if not unsignedRaiders:
-        msg = ['Try not to miss me too much.']
-    else:
-        msg = ['Hmmm, you\'re in trouble now:\n']
-    for ur in unsignedRaiders:
-        msg.extend([ur.mention if mention else ur.display_name, ' '])
-
-    return ''.join(msg)
-
-
-@bot.command(name='whipHere', help='Whip lash all unsigned members in this channel\'s recent events.')
-@is_eligible_to_whip()
-async def whipHere(ctx):
-    await ctx.message.delete()
-    log_command('whip', ctx.author.name)
-    async with ctx.channel.typing():
-        msg = await getUnsignedMembersMsg(ctx)
-        await ctx.send(msg)
-
-
-@bot.command(name='whipHereTest', help='Whip lash all unsigned members in this channel\'s recent events. Sent as DM.')
-@is_eligible_to_whip()
-async def whipHereTest(ctx):
-    await ctx.message.delete()
-    log_command('whipT', ctx.author.name)
-    async with ctx.channel.typing():
-        msg = await getUnsignedMembersMsg(ctx, mention=False)
-        await ctx.author.send(msg)
-
-
-@whipHere.error
-async def whipHere_error(ctx, error):
-    if isinstance(error, commands.CheckFailure):
-        await ctx.message.delete()
-
-
-@bot.command(name='t', help='test')
-async def t(ctx):
-    await ctx.send('I am here.')
-
-
-def ListDiff(li1, li2):
-    return list(list(set(li1) - set(li2)) + list(set(li2) - set(li1)))
-
-
-def ListSubtract(li1, li2):
-    return list(list(set(li1) - set(li2)))
-
-
-@bot.command(name='newCouncil', help='Generate new loot council based on discord ranks.')
-@is_eligible_to_whip()
-async def newCouncil(ctx):
-    await ctx.message.delete()
-    log_command('council', ctx.author.name)
-    async with ctx.channel.typing():
-        vedeni = (await commands.RoleConverter().convert(ctx, 'vedeni')).members
-        core = ListSubtract((await commands.RoleConverter().convert(ctx, 'raider')).members, vedeni)
-
-        cVedeni = random.sample(vedeni, k=councilRoleSize)
-        cCore = random.sample(core, k=councilRoleSize - 1)
-        cVedeni = [cVedeni[n].display_name for n in range(len(cVedeni))]
-        cCore = [cCore[n].display_name for n in range(len(cCore))]
-        cVedeni = cVedeni[:councilVedeniCount] + [''] + cVedeni[councilVedeniCount:]
-        cCore = cCore[:councilCoreCount] + ['', ''] + cCore[councilCoreCount:]
-
-        council = [[cVedeni[n], cCore[n]] for n in range(len(cVedeni))]
-        table = tabulate(council, headers=["Vedeni", "CoreRaiders"])
+        new_council = list(zip(c_vedeni, c_core))
+        table = tabulate(new_council, headers=["Vedeni", "CoreRaiders"])
         msg = ''.join(['```New loot council \n\n', table, '```'])
 
-        await ctx.send(msg)
+        await interaction.response.send_message(msg)
 
 
-bot.run(TOKEN)
-#    bot.send_message(bot.get_user(OWNER), 'I am dead')
+client.run(TOKEN)
